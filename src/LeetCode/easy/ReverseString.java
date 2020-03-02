9f414d989c523eb731141458af6c2469f80fdbc0
Alka Raghuvanshi <AlkaRaghuvanshi@fico.com>
2020-03-03 01:41:25 +0530

    344. Reverse String

---
+++ src/LeetCode/easy/ReverseString.java
@@ -0,0 +1,12 @@
+package LeetCode.easy;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+
+    }
+}
