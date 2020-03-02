850f2147945d55d47c088be585938a8a95492ca9
Alka Raghuvanshi <AlkaRaghuvanshi@fico.com>
2020-03-02 17:20:03 +0530

    412. Fizz Buzz

---
+++ src/LeetCode/easy/DriverClass.java
@@ -0,0 +1,8 @@
+package LeetCode.easy;
+
+public class DriverClass {
+    public static void main(String[] args) {
+        int n=15;
+        System.out.println((new FizzBuzz()).fizzBuzz(n));
+    }
+}
